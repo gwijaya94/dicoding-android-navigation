9fbf4e6c424b5db9c7b648c6d7748d1b93706f22
Gilbert <gilbertwijaya94@gmail.com>
2021-11-29 14:20:31 +0700

    add searchbar on option app bar

--- app/src/main/java/id/gwijaya94/dicodingnavigation/OptionAppBarActivity.kt
+++ app/src/main/java/id/gwijaya94/dicodingnavigation/OptionAppBarActivity.kt
@@ -1,14 +1,35 @@
 package id.gwijaya94.dicodingnavigation
 
+import android.app.Activity
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
 import id.gwijaya94.dicodingnavigation.databinding.ActivityOptionAppBarBinding
 
 class OptionAppBarActivity : AppCompatActivity() {
     private lateinit var binding: ActivityOptionAppBarBinding
+
+    fun Fragment.hideKeyboard() {
+        view?.let { activity?.hideKeyboard(it) }
+    }
+
+    fun Activity.hideKeyboard() {
+        hideKeyboard(currentFocus ?: View(this))
+    }
+
+    fun Context.hideKeyboard(view: View) {
+        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +40,30 @@ class OptionAppBarActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu, menu)
+
+        val searchManager = getSystemService(SEARCH_SERVICE) as SearchManager
+        val searchView = menu?.findItem(R.id.search)!!.actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            /*
+            Gunakan method ini ketika search selesai atau OK
+             */
+            override fun onQueryTextSubmit(query: String): Boolean {
+                hideKeyboard()
+                Toast.makeText(this@OptionAppBarActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            /*
+            Gunakan method ini untuk merespon tiap perubahan huruf pada searchView
+             */
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
         return true
     }
 
